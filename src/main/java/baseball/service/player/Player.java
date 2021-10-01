6519be0e4ec522fa4a9878fc2a49868d03d1bbc5
오진혁 <hyeok7777@gmail.com>
2021-10-02 00:30:09 +0900

    feat(Player) : Input 기능 추가 ValidateInput을 통한 Input 유효성 검증 추가 Application 반복실행 추가

--- src/main/java/baseball/service/player/Player.java
+++ src/main/java/baseball/service/player/Player.java
@@ -1,4 +1,19 @@
 package baseball.service.player;
 
+import baseball.domain.ValidateInput;
+import nextstep.utils.Console;
+
 public class Player {
+    private ValidateInput validateInput;
+
+    public Player() {
+        validateInput = new ValidateInput();
+    }
+
+    public String getPlayerNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = Console.readLine();
+        return inputNumber;
+    }
+
 }
