6853f4e2d2a8b886ce64a3c07668bc0ecae38746
오진혁 <hyeok7777@gmail.com>
2021-10-03 16:04:46 +0900

    test(BaseballGameTest) : 단위 테스트 추가

--- src/test/java/baseball/service/BaseballGameTest.java
+++ src/test/java/baseball/service/BaseballGameTest.java
@@ -2,7 +2,7 @@ package baseball.service;
 
 import baseball.domain.NumberCreator;
 import baseball.domain.ValidateInput;
-import baseball.service.player.Player;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,13 +24,34 @@ class BaseballGameTest {
     @Test
     public void inputErrorType1() {
         String playerNumber = "101";
-        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> validateInput.checkValidate(playerNumber));
+        Assertions.assertThat(validateInput.checkValidate(playerNumber)).isEqualTo(false);
     }
 
     @DisplayName("세자리 숫자 이외의 숫자 입력")
     @Test
     public void inputErrorType2() {
         String playerNumber = "1233";
-        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> validateInput.checkValidate(playerNumber));
+        Assertions.assertThat(validateInput.checkValidate(playerNumber)).isEqualTo(false);
+    }
+
+    @DisplayName("입력하지 않음")
+    @Test
+    public void noInputError() {
+        String playerNumber = "";
+        Assertions.assertThat(validateInput.checkValidate(playerNumber)).isEqualTo(false);
+    }
+
+    @DisplayName("입력하지 않음")
+    @Test
+    public void noNumeralError() {
+        String playerNumber = "abc";
+        Assertions.assertThat(validateInput.checkValidate(playerNumber)).isEqualTo(false);
+    }
+
+    @DisplayName("중복 된 숫자 입력")
+    @Test
+    public void sameNumberError() {
+        String playerNumber = "111";
+        Assertions.assertThat(validateInput.checkValidate(playerNumber)).isEqualTo(false);
     }
 }
